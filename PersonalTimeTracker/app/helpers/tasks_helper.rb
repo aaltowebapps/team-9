73deb2546713a7c9377a22279834e4bd894292f5
Mikael Koponen <mikael.koponen@gmail.com>
2012-03-27 23:46:21 +0300

    Raise seconds treshold to one minute

--- PersonalTimeTracker/app/helpers/tasks_helper.rb
+++ PersonalTimeTracker/app/helpers/tasks_helper.rb
@@ -1,7 +1,7 @@
 module TasksHelper
   def human_date(seconds)
     return nil unless seconds
-    if seconds < 15
+    if seconds < 60
       "#{seconds.round} seconds"
     else
       distance_of_time_in_words(seconds, 0, true)
