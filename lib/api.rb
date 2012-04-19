e1d386739ca8e2895ed53a252c422e46d98d26f0
Mikael Koponen <mikael.koponen@gmail.com>
2012-04-19 22:46:16 +0300

    Cache responses with redis

--- lib/api.rb
+++ lib/api.rb
@@ -1,5 +1,6 @@
 require "json"
 require "yaml"
+require "redis"
 require "savon"
 require "sinatra/base"
 require "active_support/core_ext"
@@ -10,26 +11,31 @@ class API < Sinatra::Base
   WS_URL = "http://stp.gofore.com/sujuvuus/ws/"
 
   types = %w(journey_time traffic_fluency day_data average_day_data lam_data free_flow_speeds road_weather road_station_statuses camera_presets)
+  redis = Redis.new
+
+  before do
+    content_type :json
+  end
 
   get "/" do
-    content_type :json
-    types.to_json
+    types.map{ |t| "http://#{request.host_with_port}/api/#{t}" }.to_json
   end
 
   types.each do |type|
-
-    get "/#{type}" do
-      client = Savon::Client.new do
-        wsdl.document = "#{WS_URL + type.camelize(:lower)}?wsdl"
-        http.auth.basic(SETTINGS["username"], SETTINGS["password"])
+    get "/" + type do
+      unless (response = redis.get(type))
+        response = savon_client.request(type).to_json
+        redis.set(type, response)
       end
-
-      response = client.request(type)
-
-      content_type :json
-      response.to_json
+      response
     end
+  end
 
+  def savon_client
+    Savon::Client.new do
+      wsdl.document = "#{WS_URL + type.camelize(:lower)}?wsdl"
+      http.auth.basic(SETTINGS["username"], SETTINGS["password"])
+    end
   end
 
 end
