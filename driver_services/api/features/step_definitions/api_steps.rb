13cdad7d41df6a0d59c639c99857d1eb5ee8fd22
Mikael Koponen <mikael.koponen@gmail.com>
2012-05-10 21:13:57 +0300

    Add further testing tools

--- driver_services/api/features/step_definitions/api_steps.rb
+++
@@ -1,9 +0,0 @@
-When /^I visit "([^\"]*)"$/ do |page_name|
-  visit(page_name)
-end
-
-Given /^the following weather stations exist:$/ do |table|
-  table.hashes.each do |attributes|
-    FactoryGirl.create(:weather_station, attributes)
-  end
-end
--- driver_services/api/features/support/paths.rb
+++ driver_services/api/features/support/paths.rb
@@ -11,11 +11,26 @@ module NavigationHelpers
     when /^the home\s?page$/
       '/'
 
+
+   when /^#{capture_model}(?:'s)? page$/                           # eg. the forum's page
+     path_to_pickle $1
+
+   when /^#{capture_model}(?:'s)? #{capture_model}(?:'s)? page$/   # eg. the forum's post's page
+     path_to_pickle $1, $2
+
+   when /^#{capture_model}(?:'s)? #{capture_model}'s (.+?) page$/  # eg. the forum's post's comments page
+     path_to_pickle $1, $2, :extra => $3                           #  or the forum's post's edit page
+
+   when /^#{capture_model}(?:'s)? (.+?) page$/                     # eg. the forum's posts page
+     path_to_pickle $1, :extra => $2                               #  or the forum's edit page
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #
     #   when /^(.*)'s profile page$/i
     #     user_profile_path(User.find_by_login($1))
+
+     # the following are examples using path_to_pickle
+
 
     else
       begin
