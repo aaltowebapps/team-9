9a8eb20721dcdd1a5c02fbee63cd20bd0148ac8f
Mikael Koponen <mikael.koponen@gmail.com>
2012-05-19 23:33:38 +0300

    Compile static assets

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,1 +1,1 @@
-((function(){window.App={Models:{},Collections:{},Views:{},Routers:{},init:function(){return console.log("Initialized Backbone application"),new App.Routers.Main,Backbone.history.start()}},$(document).ready(function(){return App.init()})})).call(this)
+((function(){window.App={Models:{},Collections:{},Views:{},Routers:{},init:function(){var a;return a=new App.Routers.Main,Backbone.history.start()}},$(document).ready(function(){return App.init()})})).call(this)
