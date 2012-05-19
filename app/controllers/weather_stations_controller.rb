f35f7924ee2b92722ff2fda82fea3d60eea91c4e
Mikael Koponen <mikael.koponen@gmail.com>
2012-05-19 16:51:00 +0300

    Everything worked as intended, duh

--- app/controllers/weather_stations_controller.rb
+++ app/controllers/weather_stations_controller.rb
@@ -2,13 +2,12 @@ class WeatherStationsController < ApplicationController
 
   def index
     example = WeatherStation.new(params[:weather_station])
-    logger.debug example
     @weather_stations = WeatherStation.find_all_by_example(example)
                                       .excludes(location: [0, 0], observation_data: nil)
                                       .order_by([[:station_number, :asc]])
                                       .page(params[:page])
 
-    render :json => WeatherStation.all.map {|c| WeatherStationArraySerializer.new(c) }.to_json
+    render :json => @weather_stations.map {|c| WeatherStationArraySerializer.new(c) }.to_json
   end
 
   def show
