e3d231a5a5b703510ca5100a0b70f21e5dbdc8f8
Mikael Koponen <mikael.koponen@gmail.com>
2012-05-19 16:43:13 +0300

    Debug

--- app/controllers/weather_stations_controller.rb
+++ app/controllers/weather_stations_controller.rb
@@ -2,12 +2,16 @@ class WeatherStationsController < ApplicationController
 
   def index
     example = WeatherStation.new(params[:weather_station])
+    logger.debug example
     @weather_stations = WeatherStation.find_all_by_example(example)
                                       .excludes(location: [0, 0], observation_data: nil)
                                       .order_by([[:station_number, :asc]])
                                       .page(params[:page])
 
-    raise @weather_stations.count.to_s
+    logger.debug @weather_stations.count
+
+    logger.debug @weather_stations.inspect
+
 
     render :json => @weather_stations.map {|c| WeatherStationArraySerializer.new(c) }.to_json
   end
